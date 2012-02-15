bfa5e494cd7c9c80edb36d9023ebe6974a00446c
Thomas Rabaix <thomas.rabaix@ekino.com>
2012-02-15 15:36:29 +0100

    Fix failing test

--- Tests/Invalidation/RecorderTest.php
+++ Tests/Invalidation/RecorderTest.php
@@ -44,14 +44,16 @@ class RecorderTest extends \PHPUnit_Framework_TestCase
         $m1 = new Recorder_Model_1();
         $m2 = new Recorder_Model_2();
         $recorder = new Recorder($collection);
+
+        $recorder->push();
+
         $recorder->add($m1);
         $recorder->add($m2);
 
-        $this->assertArrayHasKey('Sonata\CacheBundle\Tests\Invalidation\Recorder_Model_1', $recorder->get());
-        $this->assertArrayHasKey('Sonata\CacheBundle\Tests\Invalidation\Recorder_Model_2', $recorder->get());
+        $keys = $recorder->pop();
 
-        $this->assertEquals(array('0' => 1), $recorder->get('Sonata\CacheBundle\Tests\Invalidation\Recorder_Model_1'));
-        $recorder->reset();
-        $this->assertEquals(array(), $recorder->get('Sonata\CacheBundle\Tests\Invalidation\Recorder_Model_1'));
+        $this->assertArrayHasKey('Sonata\CacheBundle\Tests\Invalidation\Recorder_Model_1', $keys);
+        $this->assertArrayHasKey('Sonata\CacheBundle\Tests\Invalidation\Recorder_Model_2', $keys);
+
     }
 }
