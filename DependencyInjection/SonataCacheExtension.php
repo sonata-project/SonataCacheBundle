sonata-project/SonataCacheBundle

commit fd415a42012d6e996de0d34753aac72134a3f727

use connection config instate of hardcoded connection name

DependencyInjection/SonataCacheExtension.php
@@ -46,6 +46,7 @@ class SonataCacheExtension extends Extension
         $loader->load('cache.xml');
 
         $this->configureInvalidation($container, $config);
+        $this->configureORM($container, $config);
         $this->configureCache($container, $config);
     }
 
@@ -66,6 +67,22 @@ class SonataCacheExtension extends Extension
         }
 
         $cacheManager->addMethodCall('setRecorder', array(new Reference($config['cache_invalidation']['recorder'])));
+    }
+
+    /**
+     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
+     * @param $config
+     * @return void
+     */
+    public function configureORM(ContainerBuilder $container, $config)
+    {
+        $cacheManager = $container->getDefinition('sonata.cache.orm.event_subscriber');
+
+        $connections = array_keys($container->getParameter('doctrine.connections'));
+        foreach ($connections as $conn)
+        {
+            $cacheManager->addTag('doctrine.event_subscriber', array('connection' => $conn));
+        }
     }
 
     /**

Resources/config/orm.xml
@@ -12,7 +12,6 @@
         </service>
 
         <service id="sonata.cache.orm.event_subscriber" class="Sonata\CacheBundle\Invalidation\DoctrineORMListenerContainerAware">
-            <tag name="doctrine.event_subscriber" connection="default" />
             <argument type="service" id="service_container" />
             <argument>sonata.cache.orm.event_subscriber.default</argument>
         </service>

commit 1d49378cf0744a71605b6a3b8aec7aa3d94450f5

fix regression of working without the orm (b1443266) introduced by fd415a42

DependencyInjection/SonataCacheExtension.php
@@ -40,19 +40,23 @@ class SonataCacheExtension extends Extension
         $config = $processor->processConfiguration($configuration, $configs);
 
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        if (class_exists('Doctrine\\ORM\\Version')) {
+        $useOrm = class_exists('Doctrine\\ORM\\Version');
+        if ($useOrm) {
             $loader->load('orm.xml');
         }
         $loader->load('cache.xml');
 
         $this->configureInvalidation($container, $config);
-        $this->configureORM($container, $config);
+        if ($useOrm) {
+            $this->configureORM($container, $config);
+        }
         $this->configureCache($container, $config);
     }
 
     /**
-     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
+     * @param ContainerBuilder $container
      * @param $config
+     *
      * @return void
      */
     public function configureInvalidation(ContainerBuilder $container, $config)
@@ -70,8 +74,9 @@ class SonataCacheExtension extends Extension
     }
 
     /**
-     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
+     * @param ContainerBuilder $container
      * @param $config
+     *
      * @return void
      */
     public function configureORM(ContainerBuilder $container, $config)
@@ -85,10 +90,12 @@ class SonataCacheExtension extends Extension
     }
 
     /**
-     * @throws \RuntimeException
-     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
+     * @param ContainerBuilder $container
      * @param $config
+     *
      * @return void
+     *
+     * @throws \RuntimeException if the Mongo or Memcached library is not installed
      */
     public function configureCache(ContainerBuilder $container, $config)
     {