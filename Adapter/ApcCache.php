bf35ae3a7e737a5dd03d44f3a694a3d0f1062124
Thomas Rabaix <thomas.rabaix@sonata-project.org>
2014-02-14 15:16:19 +0100

    Add missing action code

--- Adapter/ApcCache.php
+++ Adapter/ApcCache.php
@@ -12,6 +12,8 @@
 namespace Sonata\CacheBundle\Adapter;
 
 use Sonata\Cache\Adapter\Cache\ApcCache as BaseApcCache;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\Routing\RouterInterface;
 
 class ApcCache extends BaseApcCache
@@ -48,5 +50,28 @@ class ApcCache extends BaseApcCache
     protected function getUrl()
     {
         $this->router->generate('sonata_cache_apc', array('token' => $this->token));
+    }
+
+    /**
+     * Cache action
+     *
+     * @param string $token A configured token
+     *
+     * @return Response
+     *
+     * @throws AccessDeniedHttpException
+     */
+    public function cacheAction($token)
+    {
+        if ($this->token == $token) {
+            apc_clear_cache('user');
+
+            return new Response('ok', 200, array(
+                'Cache-Control'  => 'no-cache, must-revalidate',
+                'Content-Length' => 2, // to prevent chunked transfer encoding
+            ));
+        }
+
+        throw new AccessDeniedHttpException('invalid token');
     }
 }
