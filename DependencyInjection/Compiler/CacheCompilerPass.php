e34755e9416fc5a7341dcb91390a0abaf100c6fd
Thomas Rabaix <thomas.rabaix@gmail.com>
2015-08-06 16:46:38 +0200

    Fix CS

--- DependencyInjection/Compiler/CacheCompilerPass.php
+++ DependencyInjection/Compiler/CacheCompilerPass.php
@@ -21,7 +21,7 @@ use Symfony\Component\DependencyInjection\Reference;
 class CacheCompilerPass implements CompilerPassInterface
 {
     /**
-     * {@inheritDoc}
+     * {@inheritdoc}
      */
     public function process(ContainerBuilder $container)
     {
